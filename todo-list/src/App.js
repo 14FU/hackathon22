4bdfb481a41c154c67193bdef5dc7a9e50ba7743
14FU <fauzia53.u@gmail.com>
2022-10-25 11:25:06 +0100

    to do list inputs created need to save the inputs

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
 import "./App.css";
+import {useState} from "react";
 // import AddUIItem from "./TodoList";
 
 const App = () => {
-  
+  const [todo,SetTodo] = useState("");
+  const[todos,setTodos]= useState([]);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    if (todo !==''){
+      setTodos([{id: `${todo}-${Date.now()}`,todo},...todos])
+    }
+
+  };
   return(
-   <div clasname="App">
-     <div classname = "border">
-     <h1>My Todo List</h1>
-        <div classname = "adding">
-          <form>
-          <input type ="text"/>
-          <button>Go</button> 
+   <div clasName="App">
+     <div className = "border">
+       <h1 className = "title">Todo List</h1>
+        <div className = "adding">
+          <form clasName ="inputTodo" onSubmit={handleSubmit}>
+            <input type ="text" onChange={(e)=>SetTodo(e.target)}/>
+           <button>Go</button> 
           </form>
-          <ul>
-            <li>
-              <span>Learn React</span>
+          <ul className = "inputDisp">
+            {todos.map((t)=>(
+            <li className= "groupDisp">
+              <span className="titleDisp">{t.todo}</span>
               <button>Edit</button>
               <button>Delete</button>
             </li>
+            ))}
           </ul>
           <form>
             
